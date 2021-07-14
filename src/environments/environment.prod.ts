a7c72f7db342a2de6c922c48df1fd80dba1bdd61
Emerson Ribeiro <emersonrjr03@gmail.com>
2021-07-15 00:11:10 +0100

    finishing the design of the home page.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8080/',
+  whitelistedDomains: ['localhost:8080', 'http://localhost:8080'],
+  blacklistedRoutes: ['localhost:8080/auth', 'http://localhost:8080/auth']
 };
